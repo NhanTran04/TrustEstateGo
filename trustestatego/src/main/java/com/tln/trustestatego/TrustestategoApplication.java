5511b6721708a783faa1274f485c11cb60963b6b
Nhan Tran <nhantran.011004@gmail.com>
2025-07-20 16:25:49 +0700

    project

---
+++ trustestatego/src/main/java/com/tln/trustestatego/TrustestategoApplication.java
@@ -0,0 +1,13 @@
+package com.tln.trustestatego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustestategoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustestategoApplication.class, args);
+	}
+
+}
---
+++ trustestatego/src/test/java/com/tln/trustestatego/TrustestategoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tln.trustestatego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustestategoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
