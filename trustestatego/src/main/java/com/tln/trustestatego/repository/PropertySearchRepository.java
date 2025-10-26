9a5f3b79c7a21c7a230c0faf8bd4b2ff003de7fd
Nhan Tran <nhantran.011004@gmail.com>
2025-10-26 14:09:36 +0700

    update

--- trustestatego/src/main/java/com/tln/trustestatego/repository/PropertySearchRepository.java
+++ trustestatego/src/main/java/com/tln/trustestatego/repository/PropertySearchRepository.java
@@ -2,14 +2,17 @@ package com.tln.trustestatego.repository;
 
 import com.tln.trustestatego.document.PropertyDocument;
 import com.tln.trustestatego.dto.response.PropertyResponse;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
-
+@Repository
+@ConditionalOnProperty(prefix = "app.elastic", name = "enabled", havingValue = "true")
 public interface PropertySearchRepository extends ElasticsearchRepository<PropertyDocument, String> {
 //    Page<PropertyResponse> findByTitleContainingIgnoreCaseOrLocationContainingIgnoreCaseAndPriceBetween
 //            (Map<String, String> params);
